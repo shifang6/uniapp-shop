88695bac70c337bb1333fb856861906b6d1903a1
shifang6 <mynameisshifang@163.com>
2022-04-21 16:30:14 +0800

    feat:首页基础布局

--- api/home.js
+++ api/home.js
@@ -1,9 +1,26 @@
 import request from '@/util/request';
 
-//资产关联属性查询
-export function GetSwiperApi (){
- return request({
-  url:`/api/public/v1/home/swiperdata`,
-  method: 'GET',
- })
+//轮播图数据接口
+export function GetSwiperApi() {
+    return request({
+
+        url: `/api/public/v1/home/swiperdata`,
+        method: 'GET',
+
+    })
+}
+// 栏目列表数据
+export function GetTopCatelogApi() {
+    return request({
+        url: '/api/public/v1/home/catitems',
+        method: 'GET'
+    })
+}
+
+// 获取楼层接口
+export function GetFloorDateApi() {
+    return request({
+        url: '/api/public/v1/home/floordata',
+        method: 'GET'
+    })
 }
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,31 +1,95 @@
 <template>
     <view>
+        <!-- 轮播图部分 -->
         <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
             <swiper-item v-for="item in swipersDate" :key="item.id">
                 <navigator class="swiper-item" :url="'/subpkg/goods_details/goods_details? goods_id=' + item.goods_id"><image :src="item.image_src"></image></navigator>
             </swiper-item>
         </swiper>
+        <!-- 顶部分类部分 -->
+        <view class="catelog-list">
+            <view class="catelog-list-item" v-for="(item, i) in cateLogDate" :key="i" @click="handleCateLogChannel(item)">
+                <image :src="item.image_src" mode="widthFix"></image>
+            </view>
+        </view>
+        <!-- 楼层数据渲染 -->
+        <view class="floor-list">
+            <view class="floor-list-item" v-for="(item, index) in floorList" :key="index">
+                <image :src="item.floor_title.image_src" mode="widthFix"></image>
+                <view class="floor-list-item-content">
+                    <view class="floor-list-item-content-left" @click="handleGoodsList(item.product_list[0])">
+                        <image :src="item.product_list[0].image_src" :style="{ width: item.product_list[0].image_width + 'rpx' }" mode="widthFix"></image>
+                    </view>
+                    <view class="floor-list-item-content-right">
+                        <image
+                            v-for="(item2, index2) in item.product_list"
+                            :key="index2"
+                            v-if="index2 !== 0"
+                            :src="item2.image_src"
+                            :style="{ width: item2.image_width + 'rpx' }"
+                            mode="widthFix"
+                            @click="handleGoodsList(item2)"
+                        ></image>
+                    </view>
+                </view>
+            </view>
+        </view>
     </view>
 </template>
 
 <script>
-import { GetSwiperApi } from '@/api/home.js';
+import { GetSwiperApi, GetTopCatelogApi, GetFloorDateApi } from '@/api/home.js';
 
 export default {
     data() {
         return {
-            swipersDate: []
+            swipersDate: [],
+            cateLogDate: [],
+            floorList: []
         };
     },
     onReady() {
         this.GetSwipersDate();
+        this.GetCateLogDate();
+        this.GetFloorList();
     },
     methods: {
         // 获取轮播图数据
         async GetSwipersDate() {
             await GetSwiperApi().then(res => {
-                console.log(res, '查看轮播图');
+                // console.log(res, '查看轮播图');
                 this.swipersDate = res.message;
+            });
+        },
+        // 获取栏目列表数据
+        GetCateLogDate() {
+            GetTopCatelogApi().then(res => {
+                // console.log(res, '查看当前的TopCatelogs');
+                this.cateLogDate = res.message;
+            });
+        },
+        // 获取楼层数据值
+        GetFloorList() {
+            GetFloorDateApi().then(res => {
+                console.log(res, '查看楼层数据');
+                for (let floor = 0; floor < res.message.length; floor++) {
+                    for (let item = 0; item < res.message[floor].product_list.length; item++) {
+                        let labels = res.message[floor].product_list[item];
+                        labels.routerUrl = '/subpkg/goods_lists/goods_lists?' + labels.navigator_url.split('?')[1];
+                    }
+                }
+                this.floorList = res.message;
+            });
+        },
+        // 打开栏目的页面
+        handleCateLogChannel({ name }) {
+            console.log(name);
+            if (name == '分类') wx.switchTab({ url: '/pages/cate/cate' });
+        },
+        // 打开产品列表页面数据
+        handleGoodsList({ routerUrl: url }) {
+            wx.navigateTo({
+                url
             });
         }
     }
@@ -39,6 +103,33 @@ swiper {
     image {
         width: 100%;
         height: 100%;
+    }
+}
+.catelog-list {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    padding: 44rpx 48rpx;
+    &-item {
+        width: 120rpx;
+        image {
+            width: 120rpx;
+        }
+    }
+}
+.floor-list {
+    padding: 0rpx 12rpx;
+    &-item {
+        &-content {
+            display: flex;
+            justify-content: space-between;
+            &-right {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-around;
+            }
+        }
     }
 }
 </style>
