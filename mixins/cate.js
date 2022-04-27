b175fd322bebc6655fbe6a814825608e2a369e85
shifang6 <mynameisshifang@163.com>
2022-04-27 16:22:30 +0800

    fix：修复购物车bar总数显示不正确的问题

--- mixins/cate.js
+++ mixins/cate.js
@@ -1,22 +1,29 @@
-import {
-    mapState,
-    mapMutations,
-    mapGetters
-} from 'vuex';
+import { mapState, mapMutations, mapGetters } from "vuex";
 export default {
     data() {},
-    onShow(){
+    onShow() {
         this.SetBarCount();
     },
     computed: {
-        ...mapGetters('m_cate', ['shop_total'])
+        ...mapGetters("m_cate", ["shop_total"]),
+    },
+    watch: {
+        shop_total: {
+            handler(newValues) {
+                uni.setTabBarBadge({
+                    index: 2,
+                    text: newValues + "",
+                });
+            },
+            immediate: true,
+        },
     },
     methods: {
         SetBarCount() {
             uni.setTabBarBadge({
                 index: 2,
-                text: this.shop_total + ''
+                text: this.shop_total + "",
             });
-        }
-    }
-}
+        },
+    },
+};
